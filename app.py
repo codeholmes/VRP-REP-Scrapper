5e4ef5ba452dbe0c59e2cb200ceeefd2b06d05b9
Anish Prasad <anishweber@icloud.com>
2023-02-12 00:49:08 +0530

    download_file() created

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ class VRP_REP_Scrapper:
         self.driver = webdriver.Firefox()
         self.driver.get("http://www.vrp-rep.org/datasets.html")
         self.wait = WebDriverWait(self.driver, 10)
+        self.download_interval = 10
+        self.switch_page_interval = 5
 
     def set_option_to_max(self):
         """Show max entries"""
@@ -26,19 +28,30 @@ class VRP_REP_Scrapper:
 
     def download_file(self):
         """Download the file"""
-        # To do
+        table_rows = self.driver.find_elements(By.XPATH, "//table/tbody/tr")
+        i = 0
+        while i < len(table_rows):
+            row = table_rows[i]
+            download_links = row.find_element(
+                By.XPATH, "./td/a[contains(@href,'download')]"
+            )
+            download_links.click()
+            time.sleep(self.download_interval)
+            i += 1
+            # refinding the element after each download
+            # table_rows = self.driver.find_elements(By.XPATH, "//table/tbody/tr")
 
     def move_to_next_page(self, page_no):
         """Next page clicker!"""
-        time.sleep(2)
         try:
             next_page = self.wait.until(
                 EC.element_to_be_clickable((By.ID, "datatable_next"))
             )
-            # download_file()
+            self.download_file()
             if "disabled" not in next_page.get_attribute("class"):
                 a_link = next_page.find_element(By.XPATH, "//a[text()='Next']")
                 a_link.click()
+                time.sleep(self.switch_page_interval)
                 page_no += 1
                 print("Moving to Page:", page_no)
                 self.move_to_next_page(page_no)
@@ -55,4 +68,6 @@ class VRP_REP_Scrapper:
 
 if __name__ == "__main__":
     scrapper = VRP_REP_Scrapper()
-    scrapper.set_option_to_max()
+    # scrapper.set_option_to_max()
+    # scrapper.download_file()
+    scrapper.move_to_next_page(1)
